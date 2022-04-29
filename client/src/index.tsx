d773b5431145ad13fcb358f0f6cb40bd84194986
Jihad El Sayed <david.pales1@gmail.com>
2022-04-29 16:19:13 +0200

    dd

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< Updated upstream
 import reportWebVitals from './reportWebVitals';
 import { ReactQueryProvider } from './lib/react-query';
 import { AuthProvider } from './lib/auth';
@@ -17,9 +18,77 @@ root.render(
       </AuthProvider>
     </ReactQueryProvider>
   </React.StrictMode>
-);
+=======
+//import reportWebVitals from './reportWebVitals';
+import { ReactQueryProvider } from './lib/react-query';
+import { AuthProvider } from './lib/auth';
+import { initMock } from "./mock";
+import { render } from "react-dom";
 
+
+import { BrowserRouter } from "react-router-dom";
+
+//import * as serviceWorker from "./serviceWorker";
+
+
+
+
+//serviceWorker.unregister();
+
+/** 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+>>>>>>> Stashed changes
+);
+root.render(
+  <React.StrictMode>
+    <ReactQueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ReactQueryProvider>
+  </React.StrictMode>
+);*/
+function Application() {
+  return (
+    <BrowserRouter>
+    <App />
+  </BrowserRouter>
+    // <ReactQueryProvider>
+    //   <AuthProvider>
+    //     <App />
+    //   </AuthProvider>
+    // </ReactQueryProvider>
+  );
+}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+const rootElement = document.getElementById("root");
+//initMock.then(() => {
+  render(<Application />, rootElement);
+//});
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
+
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
+<<<<<<< Updated upstream
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+=======
+// serviceWorker.unregister();
+>>>>>>> Stashed changes
